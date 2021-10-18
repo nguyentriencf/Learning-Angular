6c39311015b3307673f0efc163bef2049277a6d4
Nguyen Trien <trienvaj@gmail.com>
2021-10-18 16:10:01 +0700

    create project in angular

---
+++ first-project/src/app/hello/hello.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  template:'hello page',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
